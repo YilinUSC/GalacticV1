765902c5306c78b8efaef3f22bfe8b943c08532d
Unnati Babruwad <40277323+babruwadunnati@users.noreply.github.com>
2023-09-21 09:33:51 -0700

    Added Number to the enemies and enemies from 0-9 are generated.
    Handled the stray bullet issue

--- Assets/EnemyNumber.cs
+++ Assets/EnemyNumber.cs
@@ -1,21 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class EnemyNumber : MonoBehaviour
 {
-    
+    //public GameObject FloatingTextPrefab;
+    public TextMeshPro textnumber;
     public int number;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        number = Random.Range(0, 9);
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        textnumber.text = number.ToString();
+        
+
+        
     }
+    
 }
